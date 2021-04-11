f14335152a3f18873fc785964fa3246af747ebd2
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-04-11 17:52:35 +0000

    Create service Implementation for Dependency Injection without Spring Framework:

---
+++ src/main/java/com/hellobramah/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.hellobramah.sfgdi.services;
+
+public class GreetingServiceImpl implements GreetingService {
+    @Override
+    public String announceGreetings() {
+        return "Hello my beautiful world";
+    }
+}
