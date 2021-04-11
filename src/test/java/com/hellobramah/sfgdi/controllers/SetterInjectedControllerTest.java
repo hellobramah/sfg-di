ccfd729e46769db794094e611fa1ca1214f85147
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-04-11 17:53:52 +0000

    Create JUnit5 tests for Dependency Injection without Spring Framework:

---
+++ src/test/java/com/hellobramah/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package com.hellobramah.sfgdi.controllers;
+
+import com.hellobramah.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+private SetterInjectedController controller;
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.print(controller.getGreeting());
+    }
+}
