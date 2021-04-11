74085f23479b7c75225ac772e0ce0adba0fef3ae
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-04-11 17:59:48 +0000

    Adapt main class for controllers without Spring Framework

--- src/main/java/com/hellobramah/sfgdi/SfgDiApplication.java
+++ src/main/java/com/hellobramah/sfgdi/SfgDiApplication.java
@@ -1,13 +1,19 @@
 package com.hellobramah.sfgdi;
 
+import com.hellobramah.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+		ApplicationContext appCon =SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController) appCon.getBean("myController");
+		String greet = myController.greetings();
+		System.out.println(greet);
 	}
 
 }
