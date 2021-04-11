15d0038ff70285792868c0577eefa1d91d69d1c9
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-04-11 17:48:02 +0000

    Dependency Injection without Spring Framework: Use property, this is the least prefered method

---
+++ src/main/java/com/hellobramah/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,12 @@
+package com.hellobramah.sfgdi.controllers;
+
+import com.hellobramah.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    public GreetingService greetingService;
+
+    public String getGreeting(){
+     return   greetingService.announceGreetings();
+    }
+}
