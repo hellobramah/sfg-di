8e79d4ffef2d8ffbe8b45e8f072fe248be82bf7e
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-04-28 04:00:45 +0000

    Use Primary bean

--- src/main/java/com/hellobramah/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/com/hellobramah/sfgdi/services/PrimaryGreetingService.java
@@ -3,7 +3,7 @@ package com.hellobramah.sfgdi.services;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-
+//Using the primaray bean
 @Primary
 @Service
 public class PrimaryGreetingService implements GreetingService{
