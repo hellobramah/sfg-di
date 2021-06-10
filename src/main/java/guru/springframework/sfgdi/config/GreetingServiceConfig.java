7f67b28ce55544df3f3bf835257dba181a2df353
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-06-10 07:14:34 +0000

    Using properties source

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,77 @@
+package guru.springframework.sfgdi.config;
+
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+import guru.springframework.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+
+@PropertySource("classpath:datasource.properties")
+@ImportResource("classpath:sfgdi-config.xml")
+@Configuration
+public class GreetingServiceConfig {
+
+
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${hellobramah.username}") String username,
+                                  @Value("${hellobramah.password}") String password,
+                                  @Value("${hellobramah.jdbcUrl}") String jdbcUrl){
+        FakeDataSource fakeDataSource=new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcUrl(jdbcUrl);
+        return fakeDataSource;
+    }
+
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return  new PetServiceFactory();
+    }
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory().getPetType("dog");
+    }
+    @Profile("cat")
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory().getPetType("cat");
+    }
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService(){
+        return new I18NSpanishService();
+    }
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
+    }
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+   /* @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }*/
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return  new SetterInjectedGreetingService();
+    }
+}
